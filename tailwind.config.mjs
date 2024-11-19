4fb4b43496f2fb05990b55abef4af1c5b38d0292
Patrick Youssef <patricksyoussef@gmail.com>
2024-11-19 15:09:31 -0800

    Petrona Font Replaces Lora

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				default: "#FEFDFC",
-			},
-			fontFamily: {
-				serif: ['"Lora"', ...defaultTheme.fontFamily.serif],
-			},
-			maxWidth: {
-				"base": "1500px"
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        default: "#FEFDFA",
+      },
+      fontFamily: {
+        serif: ['"Petrona Variable"', ...defaultTheme.fontFamily.serif],
+      },
+      maxWidth: {
+        base: "1500px",
+      },
+    },
+  },
+  plugins: [],
+};
