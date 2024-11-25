8431048a57f0c750fc92fe0d8e4d1ea94d392ff3
Patrick Youssef <patricksyoussef@gmail.com>
2024-11-25 14:25:37 -0800

    Common Tailwind Configs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 import defaultTheme from "tailwindcss/defaultTheme";
+const colors = require("tailwindcss/colors");
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -9,12 +10,16 @@ export default {
       colors: {
         background: "#FEFDFA",
         accent: "#2563eb",
+        edge: colors.gray[500],
       },
       fontFamily: {
         sans: ['"DM Sans Variable"', ...defaultTheme.fontFamily.serif],
       },
       maxWidth: {
         base: "1500px",
+      },
+      spacing: {
+        common: "0.75rem",
       },
     },
   },
