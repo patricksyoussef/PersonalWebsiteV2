9eca6abda5e125ad82c108d8e9c2113a76931693
Patrick Youssef <patricksyoussef@gmail.com>
2025-01-24 01:21:48 -0800

    Adding Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,23 +5,21 @@ import { astroExpressiveCode } from "astro-expressive-code";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      nesting: true,
-    }),
-    astroExpressiveCode({
-      themes: ["light-plus"],
-      styleOverrides: {
-        // You can optionally override the plugin's default styles here
-        frames: {
-          editorBackground: "rgb(247, 247, 247)",
-        },
+  integrations: [tailwind({
+    nesting: true,
+  }), astroExpressiveCode({
+    themes: ["light-plus"],
+    styleOverrides: {
+      // You can optionally override the plugin's default styles here
+      frames: {
+        editorBackground: "rgb(247, 247, 247)",
       },
-    }),
-    mdx(),
-  ],
+    },
+  }), mdx(), preact()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
