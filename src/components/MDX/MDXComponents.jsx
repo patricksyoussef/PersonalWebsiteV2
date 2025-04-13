b0859b09533da002438983c2ba58fd1ae848b4be
Patrick Youssef <patricksyoussef@gmail.com>
2025-04-13 01:27:40 -0700

    Lowering Font Weight on Inline Code

--- src/components/MDX/MDXComponents.jsx
+++ src/components/MDX/MDXComponents.jsx
@@ -6,7 +6,7 @@ import CodeBlock from "./CodeBlock/CodeBlock.astro";
 
 const InlineCode = (props) => {
   return (
-    <code className="bg-slate-100 border border-slate-300 text-slate-800 py-[0.025rem] px-[0.18rem] rounded-md font-mono text-[0.9em]">
+    <code className="bg-slate-100 font-normal border border-slate-300 text-slate-800 py-[0.025rem] px-[0.18rem] rounded-md font-mono text-[0.9em]">
       {props.children}
     </code>
   );
