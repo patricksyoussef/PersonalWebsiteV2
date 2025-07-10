2d22ac28c72c1c14332e8bd080a5cb47e3ecb5f1
Patrick Youssef <patricksyoussef@gmail.com>
2025-07-09 23:24:34 -0700

    Constructing Paths for Twitter Card Images

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://loquacious-snickerdoodle-c04dc5.netlify.app/",
   integrations: [
     tailwind({
       nesting: true,
